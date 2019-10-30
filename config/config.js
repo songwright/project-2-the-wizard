0ae20de92b7ae2647f7f3819b2808d0015ebef8d
Ramon Sanchez <songwright66@gmail.com>
2019-10-29 19:16:36 -0700

    Added project starter files

---
+++ config/config.js
@@ -0,0 +1,24 @@
+exports.configure = function(env) {
+  return configs[env];
+}
+
+let configs = {
+  "development": {
+    "username": "root",
+    // "password": `${process.env.DB_PASSWORD}`,
+    "password": `${process.env.DB_PASSWORD}`,
+    "database": "passport_demo",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable":"JAWSDB_URL"
+  }
+}
